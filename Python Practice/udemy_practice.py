ecdff366378bac024007f23889312bf2fd98c4b8
Taeyeon Kim <taekim1291@gmail.com>
2025-09-29 14:07:56 -0600

    udemy_pratice.py

---
+++ Python Practice/udemy_practice.py
@@ -0,0 +1,22 @@
+#len method with the input method
+# question = input("What's your name?")
+# print(len(question))
+
+# print(input("How old are you?"))
+
+# print("Welcome to the Band Name Generator.")
+# location_question = input("Where did you grow up?\n")
+# pet_name = input("What's your cat's name?\n")
+
+# print("Your band name could be: " + location_question + " " + pet_name + "!")
+
+# print(type("123"))
+# print(type(123))
+# print(type(True))
+# print(type(12.5))
+
+# len(12345) len doens't like to read integers / it prefers texts
+
+print(int(1234) + int(1234))
+print(int("1234") + int("1234"))
+print(int("one") + int("two"))
