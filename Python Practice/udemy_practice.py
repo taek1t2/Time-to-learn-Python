95145ea454de20d49105cd5d050597810de3b892
Taeyeon Kim <taekim1291@gmail.com>
2025-09-30 09:15:59 -0600

    udemy_practice.py

--- Python Practice/udemy_practice.py
+++ Python Practice/udemy_practice.py
@@ -26,11 +26,12 @@
 # str()
 # bool()
 
-bmi = 84 / 1.65 ** 2
-print(bmi)
+# bmi = 84 / 1.65 ** 2
+# print(bmi)
 
-print(int(bmi))
+# print(int(bmi))
 
-print(round(bmi))
+# print(round(bmi))
 
-print(round(bmi, 2))
+# print(round(bmi, 2))
+
