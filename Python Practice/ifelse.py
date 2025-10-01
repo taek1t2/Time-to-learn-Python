4d49dfd5ad571541f90c2408c0daba415307ff48
Taeyeon Kim <taekim1291@gmail.com>
2025-10-01 14:37:31 -0600

    treasure_hunt.py

---
+++ Python Practice/ifelse.py
@@ -0,0 +1,17 @@
+num = int(input("What number do you want to check? "))
+
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+# ORIGINAL CODE
+# x = int[]
+#
+# if num in x % 2 == 0:
+#     print("even")
+# else:
+#     print("odd")
+#
+# print(num(11))
+
