60549372adca6d9e451e3bc56a2b3179e8e3b3fa
Taeyeon Kim <taekim1291@gmail.com>
2025-10-02 15:40:27 -0600

    rock_paper_scissors.py

--- Python Practice/rock_paper_scissors.py
+++ Python Practice/rock_paper_scissors.py
@@ -25,21 +25,48 @@ scissors = '''
 ---.__(___)
 '''
 
-list_with_hands = [rock, paper, scissors]
-player1 = input("What do you choose? Type 0 for Rock, 1 for Paper, 2 for Scissors. ")
+
+game_art = [rock, paper, scissors]
+player = int(input("What do you choose? Type 0 for Rock, 1 for Paper, 2 for Scissors.\n"))
 import random
 
-choose_number = [rock, paper, scissors]
+player_choice = player
+print(f"You chose: {game_art[player_choice]}")
 
-if player1 == "0":
-    print(choose_number[0])
-    print(random.choice(list_with_hands))
-elif player1 == "1":
-    print(choose_number[1])
-    print(random.choice(list_with_hands))
-elif player1 == "2":
-    print(choose_number[2])
-    print(random.choice(list_with_hands))
-else:
-    print("Number error. Choose only 0, 1, 2")
+computer_choice = random.randint(0, 2)
+print(f"Computer chose: {game_art[computer_choice]}")
+
+if player_choice >= 3 or player_choice < 0:
+    print("Error:Please list 0, 1, 2")
+elif player_choice == computer_choice:
+    print("It's a draw!")
+elif player_choice == 0 and computer_choice == 2:
+    print("You won!!!")
+elif player_choice == 1 and computer_choice == 0:
+    print("You won!!!")
+elif player_choice == 2 and computer_choice == 1:
+    print("You won!!!")
+elif computer_choice == 0 and player_choice == 2:
+    print("You lose!!!")
+elif computer_choice == 1 and player_choice == 0:
+    print("You lose!!!")
+elif computer_choice == 2 and player_choice == 1:
+    print("You lose!!!")
+
+
+# Old code that I've approached
+
+# if player1 == "0":
+#     print(player_with_hands[0])
+#     print(random.choice(computer_art))
+# elif player1 == "1":
+#     print(player_with_hands[1])
+#     print(random.choice(computer_art))
+# elif player1 == "2":
+#     print(player_with_hands[2])
+#     print(random.choice(computer_art))
+# else:
+#     print("Number error. Choose only 0, 1, 2")
+
+
 
