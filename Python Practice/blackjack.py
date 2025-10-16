ca5a0946f3b7404769600a02d7c23076f4f59e3f
Taeyeon Kim <taekim1291@gmail.com>
2025-10-16 12:51:03 -0600

    blackjack

--- Python Practice/blackjack.py
+++ Python Practice/blackjack.py
@@ -1,13 +1,12 @@
 import random
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-# cards[0] = 1
 random.shuffle(cards)
 
 start_blackjack = input("Do you want to play a game of Blackjack? (y/n) \n").lower()
 print("\n" * 25)
 
-import blackjack_art
-print(blackjack_art.logo)
+import art
+print(art.logo)
 
 player_hand = []
 computer_hand = []
@@ -18,15 +17,16 @@ if start_blackjack == "y":
     second_card = random.choice(cards)
     player_hand.append(second_card)
     current_score = sum(player_hand)
+    
     print(f"Your cards: {player_hand}, current score: {current_score}")
 
-    computer_first_card = random.choice(cards)
-    computer_hand.append(computer_first_card)
-    hidden_display = "_"
-    another_card = random.choice(cards)
-    computer_hand.append(another_card)
-    puter_score = sum(computer_hand)
-    print(f"Dealer's first card: {[computer_hand[0], hidden_display]}\n")
+    # computer_first_card = random.choice(cards)
+    # computer_hand.append(computer_first_card)
+    # hidden_display = "_"
+    # another_card = random.choice(cards)
+    # computer_hand.append(another_card)
+    # puter_score =
+    # print(f"Dealer's first card: {[computer_hand[0], hidden_display]}\n")
 
     hit_or_stand = input('Type "hit" to get another card OR type "stand" to pass: \n').lower()
 
@@ -53,6 +53,19 @@ if start_blackjack == "y":
         computer_hand.append(draws_again)
         puter_score = sum(computer_hand)
         print(f"Dealer's hand: {computer_hand}, Dealer's score: {puter_score}")
+
+def get_accurate_score():
+    score = 0
+    num_ace = 0
+    for card in player_hand or computer_hand:
+        if card == 11:
+            num_ace += 1  # num_ace = num_ace + 1
+            score += card  # score = score + card
+
+    while score > 21 and num_ace > 0:
+        score -= 10
+        num_ace -= 1
+    return score
 
 # game_continues = True
 # while game_continues:
